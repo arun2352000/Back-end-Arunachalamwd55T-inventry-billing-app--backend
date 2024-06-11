f0741e6f314c85bd784836ccade159853c863987
arun2352000 <poorvaja10@gmail.com>
2024-06-11 11:48:30 +0530

    jwt token generation

--- Controllers/user.controllers.js
+++ Controllers/user.controllers.js
@@ -1,15 +1,18 @@
 import User from "../Models/user.schema.js";
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
+dotenv.config()
 
 export const registerUser = async(req, res)=>{
     try {
-            const {username, email, password} = req.body
+            const {username, email, password, role} = req.body
             const hashPassword = await bcrypt.hash (password ,10)
             console.log("hashPassword", hashPassword);
 
 
-            const newUser= new User({username, email, password: hashPassword})
+            const newUser= new User({username, email, password: hashPassword, role})
             await newUser.save()
 
         res.status(200).json({message: "Register Successful", data: newUser})
@@ -31,7 +34,11 @@ export const loginUser = async(req, res)=>{
             return res.status(401).json({message:"Invalid Password"})
         }
 
-        res.status(200).json({message: "login Successful"})
+        const token = jwt.sign({_id:user._id}, process.env.JWT_SECRET)
+        user.token=token
+        await user.save()
+        
+        res.status(200).json({message: "login Successful", token:token})
     } catch (error) {
         console.log(error);
         res.status(500).json({error:'Login Failed, Internal server error'})
--- Models/user.schema.js
+++ Models/user.schema.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: String,
     email: String,
-    password: String
+    password: String,
+    role: String,
+    token: String,
 
 })
 const User=mongoose.model('User', userSchema)
-export default User
+export default User 
